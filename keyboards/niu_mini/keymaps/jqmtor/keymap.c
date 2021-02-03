9c2bec0d7a7e42aaa70bd5ba69acffd1e0e2dd57
Joaquim Torres <jqmtor@protonmail.com>
2021-02-03 15:57:08 +0100

    Improve layer naming
    Use enum to name each layer and update comments of each layer
    accordingly.

--- keyboards/niu_mini/keymaps/jqmtor/keymap.c
+++ keyboards/niu_mini/keymaps/jqmtor/keymap.c
@@ -1,8 +1,14 @@
 #include QMK_KEYBOARD_H
 
+enum layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* Layer 0
+    /* Qwerty
      * ,-----------------------------------------------------------------------------------.
      * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  -   |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -10,35 +16,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   `  |   ,  |   .  |  /   |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Ctrl |  Alt |  GUI |Layer1| Bksp |    Space    |Enter |Layer2|   =  |   [  |  ]   |
+     * | Ctrl |  Alt |  GUI |Lower | Bksp |    Space    |Enter |Raise |   =  |   [  |  ]   |
      * `-----------------------------------------------------------------------------------'
      */
-    [0] = LAYOUT_planck_mit(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,   KC_Y,   KC_U,  KC_I,   KC_O,    KC_P,    KC_MINS,
-        KC_TAB,  KC_A,    KC_S,    KC_D,  KC_F,    KC_G,   KC_H,   KC_J,  KC_K,   KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,   KC_N,   KC_M,  KC_GRV, KC_COMM, KC_DOT,   KC_SLSH,
-        KC_LCTL, KC_LALT, KC_LGUI, MO(1), KC_BSPC, KC_SPC,         KC_ENT, MO(2), KC_EQL,  KC_LBRC, KC_RBRC
+    [_QWERTY] = LAYOUT_planck_mit(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,       KC_R,    KC_T,   KC_Y,   KC_U,  KC_I,        KC_O,    KC_P,    KC_MINS,
+        KC_TAB,  KC_A,    KC_S,    KC_D,       KC_F,    KC_G,   KC_H,   KC_J,  KC_K,        KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,    KC_B,   KC_N,   KC_M,  KC_GRV,      KC_COMM, KC_DOT,   KC_SLSH,
+        KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER), KC_BSPC,     KC_SPC,     KC_ENT, MO(_RAISE), KC_EQL,  KC_LBRC, KC_RBRC
     ),
 
-    /* Layer 1
+    /* Lower
      * ,-----------------------------------------------------------------------------------.
      * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |      |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      | Vol- | Vol+ | Mute |      |      |      |  F11 |  F12 |      |      |      |
+     * |      | Vol- | Vol+ | Mute |      |      | Left | Down |  Up  |Right |      |      |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |      |      |      |      |
+     * |      |      |      |      |      |  F11 |  F12 |      |      |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Reset|      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
-    [1] = LAYOUT_planck_mit(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, KC_F11,  KC_F12,  _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    [_LOWER] = LAYOUT_planck_mit(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_F9,    KC_F10,  _______,
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,
+        _______, _______, _______, _______, _______,  KC_F11, KC_F12, _______, _______, _______, _______, _______,
         RESET,   _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
     ),
 
-    /* Layer 2 (r_ Indicates RGB Controls)
+    /* Raise
      * ,-----------------------------------------------------------------------------------.
      * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -49,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
-    [2] = LAYOUT_planck_mit(
+    [_RAISE] = LAYOUT_planck_mit(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
         _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______,

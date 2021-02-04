75448b3233076f15c4cf86d0688ab96a1193353a
Joaquim Torres <jqmtor@protonmail.com>
2021-02-04 13:51:45 +0100

    Add symbols to the lower layer
    They are in the same position where the numbers usually are.

--- keyboards/niu_mini/keymaps/jqmtor/keymap.c
+++ keyboards/niu_mini/keymaps/jqmtor/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   `  |   ,  |   .  |  /   |
+     * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /   |Shift |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Ctrl |  Alt |  GUI |Lower | Bksp |    Space    |Enter |Raise |   =  |   [  |  ]   |
      * `-----------------------------------------------------------------------------------'
@@ -22,26 +22,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_planck_mit(
         KC_ESC,  KC_Q,                 KC_W,    KC_E,       KC_R,    KC_T,   KC_Y,   KC_U,  KC_I,        KC_O,                 KC_P,                  KC_MINS,
         KC_TAB,  KC_A,                 KC_S,    KC_D,       KC_F,    KC_G,   KC_H,   KC_J,  KC_K,        KC_L,                 KC_SCLN,               KC_QUOT,
-        KC_LSFT, KC_Z,                 KC_X,    KC_C,       KC_V,    KC_B,   KC_N,   KC_M,  KC_GRV,      KC_COMM,              KC_DOT,                MT(MOD_RSFT, KC_SLSH),
+        KC_LSFT, KC_Z,                 KC_X,    KC_C,       KC_V,    KC_B,   KC_N,   KC_M,  KC_COMM,     KC_DOT,               KC_SLSH,               KC_RSFT,
         KC_LCTL, MT(MOD_LALT, KC_GRV), KC_LGUI, MO(_LOWER), KC_BSPC,     KC_SPC,     KC_ENT, MO(_RAISE), MT(MOD_RGUI, KC_EQL), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
     ),
 
     /* Lower
      * ,-----------------------------------------------------------------------------------.
-     * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |      |
+     * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  |   |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |      | Vol- | Vol+ | Mute |      |      | Left | Down |  Up  |Right |      |      |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      |      |      |      |  F11 |  F12 |      |      |      |      |      |
+     * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Reset|      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
     [_LOWER] = LAYOUT_planck_mit(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_F9,    KC_F10,  _______,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,
-        _______, _______, _______, _______, _______,  KC_F11, KC_F12, _______, _______, _______, _______, _______,
-        RESET,   _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_PIPE,
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,
+        RESET,   _______, _______, _______, _______,     _______,      _______, _______, _______,  _______, _______
     ),
 
     /* Raise

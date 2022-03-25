df031069ce97b4b89ae409a60d94950cd9f159da
Joaquim Torres <joaquim.torres@nubank.com.br>
2022-03-25 10:20:25 +0100

    Add cmd+shift+number to movement 2 layer

--- keyboards/niu_mini/keymaps/jqmtor/keymap.c
+++ keyboards/niu_mini/keymaps/jqmtor/keymap.c
@@ -23,10 +23,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT_planck_mit(
-        KC_ESC,  KC_Q,                 KC_W,          KC_E,           KC_R,           KC_T,   KC_Y, KC_U,           KC_I,       KC_O,                 KC_P,                  KC_MINS,
-        KC_TAB,  KC_A,                 LT(_WND,KC_S), LT(_MV_2,KC_D), LT(_MV_1,KC_F), KC_G,   KC_H, KC_J,           KC_K,       KC_L,                 KC_SCLN,               KC_QUOT,
-        KC_LSFT, KC_Z,                 KC_X,          KC_C,           LT(_MV_1,KC_V), KC_B,   KC_N, LT(_MV_1,KC_M), KC_COMM,    KC_DOT,               KC_SLSH,               KC_RSFT,
-        KC_LCTL, MT(MOD_LALT, KC_GRV), KC_LGUI,       MO(_LOWER),     KC_BSPC,        KC_SPC,       KC_ENT,         MO(_RAISE), MT(MOD_RGUI, KC_EQL), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
+        KC_ESC,  KC_Q,                 KC_W,          KC_E,           KC_R,           KC_T,   KC_Y, KC_U,            KC_I,              KC_O,                 KC_P,                  KC_MINS,
+        KC_TAB,  KC_A,                 LT(_WND,KC_S), LT(_MV_2,KC_D), LT(_MV_1,KC_F), KC_G,   KC_H, KC_J,            KC_K,              KC_L,                 KC_SCLN,               KC_QUOT,
+        KC_LSFT, KC_Z,                 KC_X,          LT(_MV_2,KC_C), LT(_MV_1,KC_V), KC_B,   KC_N, LT(_MV_1,KC_M),  LT(_MV_2,KC_COMM), KC_DOT,               KC_SLSH,               KC_RSFT,
+        KC_LCTL, MT(MOD_LALT, KC_GRV), KC_LGUI,       MO(_LOWER),     KC_BSPC,        KC_SPC,       KC_ENT,          MO(_RAISE),        MT(MOD_RGUI, KC_EQL), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
     ),
 
     /* Lower
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,    _______,    _______,    _______,    _______,    _______,       _______,       _______,     _______,        _______,    _______,
         _______, _______,    _______,    _______,    _______,           _______,            _______,       _______,     _______,        LGUI(LSFT(KC_LBRC)), LGUI(LSFT(KC_RBRC))
     ),
-    /* Movement 2 - text + history navigation
+    /* Movement 2 - text + history navigation + number commands
      * ,-----------------------------------------------------------------------------------.
-     * |      |      |      |      |      |      |      |      |      |      |      |      |
+     * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |      |      |      |      |      |      | left | down |  up  |right |      |      |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -93,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_MV_2] = LAYOUT_planck_mit(
-        _______, _______, _______, _______, _______, _______, _______,       _______,       _______,     _______,        _______,       _______,
-        _______, _______, _______, _______, _______, _______, LGUI(KC_LEFT), LGUI(KC_DOWN), LGUI(KC_UP), LGUI(KC_RIGHT), _______,       _______,
-        _______, _______, _______, _______, _______, _______, _______,       _______,       _______,     _______,        _______,       _______,
-        _______, _______, _______, _______, _______,     _______,            _______,       _______,     _______,        LGUI(KC_LBRC), LGUI(KC_RBRC)
+        _______, LGUI(LSFT(KC_1)), LGUI(LSFT(KC_2)), LGUI(LSFT(KC_3)), LGUI(LSFT(KC_4)), LGUI(LSFT(KC_5)), LGUI(LSFT(KC_6)), LGUI(LSFT(KC_7)), LGUI(LSFT(KC_8)), LGUI(LSFT(KC_9)), LGUI(LSFT(KC_0)), _______,
+        _______, _______,          _______,          _______,          _______,          _______,          LGUI(KC_LEFT),    LGUI(KC_DOWN),    LGUI(KC_UP),      LGUI(KC_RIGHT),   _______,          _______,
+        _______, _______,          _______,          _______,          _______,          _______,          _______,          _______,          _______,          _______,          _______,          _______,
+        _______, _______,          _______,          _______,          _______,                  _______,                    _______,          _______,          _______,          LGUI(KC_LBRC),    LGUI(KC_RBRC)
     ),
     /* WND - window layout management
      * ,-----------------------------------------------------------------------------------.

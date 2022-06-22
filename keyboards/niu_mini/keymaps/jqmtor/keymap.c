6a3e5286ce6e168b16669e9c85377c54666caf61
Joaquim Torres <joaquim.torres@nubank.com.br>
2022-06-22 10:28:48 +0200

    Double-tap shift keys to activate Caps lock

--- keyboards/niu_mini/keymaps/jqmtor/keymap.c
+++ keyboards/niu_mini/keymaps/jqmtor/keymap.c
@@ -9,6 +9,25 @@ enum layers {
     _WND,
 };
 
+/*
+ * Tap Dance declarations
+ * See: See: https://docs.qmk.fm/#/feature_tap_dance
+ */
+enum {
+    TD_RSFT_CAPS,
+    TD_LSFT_CAPS
+};
+
+/*
+ * Tap Dance definitions
+ */
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for left shift, twice for Caps Lock
+    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    // Tap once for right shift, twice for Caps Lock
+    [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Qwerty
@@ -23,10 +42,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT_planck_mit(
-        KC_ESC,  KC_Q,                 KC_W,          KC_E,           KC_R,           KC_T,   KC_Y, KC_U,            KC_I,              KC_O,                 KC_P,                  KC_MINS,
-        KC_TAB,  KC_A,                 LT(_WND,KC_S), LT(_MV_2,KC_D), LT(_MV_1,KC_F), KC_G,   KC_H, KC_J,            KC_K,              KC_L,                 KC_SCLN,               KC_QUOT,
-        KC_LSFT, KC_Z,                 KC_X,          LT(_MV_2,KC_C), LT(_MV_1,KC_V), KC_B,   KC_N, LT(_MV_1,KC_M),  LT(_MV_2,KC_COMM), KC_DOT,               KC_SLSH,               KC_RSFT,
-        KC_LCTL, MT(MOD_LALT, KC_GRV), KC_LGUI,       MO(_LOWER),     KC_BSPC,        KC_SPC,       KC_ENT,          MO(_RAISE),        MT(MOD_RGUI, KC_EQL), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
+        KC_ESC,           KC_Q,                 KC_W,          KC_E,           KC_R,           KC_T,   KC_Y, KC_U,           KC_I,              KC_O,                 KC_P,                  KC_MINS,
+        KC_TAB,           KC_A,                 LT(_WND,KC_S), LT(_MV_2,KC_D), LT(_MV_1,KC_F), KC_G,   KC_H, KC_J,           KC_K,              KC_L,                 KC_SCLN,               KC_QUOT,
+        TD(TD_LSFT_CAPS), KC_Z,                 KC_X,          LT(_MV_2,KC_C), LT(_MV_1,KC_V), KC_B,   KC_N, LT(_MV_1,KC_M), LT(_MV_2,KC_COMM), KC_DOT,               KC_SLSH,               TD(TD_RSFT_CAPS),
+        KC_LCTL,          MT(MOD_LALT, KC_GRV), KC_LGUI,       MO(_LOWER),     KC_BSPC,        KC_SPC,       KC_ENT,         MO(_RAISE),        MT(MOD_RGUI, KC_EQL), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
     ),
 
     /* Lower
